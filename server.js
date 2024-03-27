1a196e16bfd3de8d3fbeb17a163fc9ba5f93b7d7
Subham Roy <itssubhamroy@gmail.com>
2024-03-27 16:46:33 +0530

    6th

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product_model");
+require("dotenv").config(); // Load environment variables from .env file
+const MONGODB_URL = process.env.MONGODB_URL; // Get MongoDB URL from environment variables
+
 const app = express();
 
 //middlewire
@@ -44,10 +47,19 @@ app.listen(PORT, () => {
   console.log("Server running at port 3000");
 });
 
+// mongoose
+//   .connect(
+//     "mongodb+srv://admin:A12345@cluster0.6uopllj.mongodb.net/BTechPapers?retryWrites=true&w=majority&appName=Cluster0"
+//   )
+//   .then(() => {
+//     console.log("DB Connected Successfully");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to DB:", err);
+//   });
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:A12345@cluster0.6uopllj.mongodb.net/BTechPapers?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("DB Connected Successfully");
   })
