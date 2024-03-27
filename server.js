6db50fdbb668ad5ff7cf717f943f8f4c87875171
Subham Roy <itssubhamroy@gmail.com>
2024-03-27 23:06:36 +0530

    11

--- server.js
+++ server.js
@@ -22,14 +22,6 @@ app.get("/about", (req, res) => {
 });
 
 // Get all products
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 app.get("/products", async (req, res) => {
   try {
